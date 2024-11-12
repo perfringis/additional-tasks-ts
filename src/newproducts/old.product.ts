2dac4290486c9341c52eca9e12ee5180e28254c6
perfringis <kopko.mateusz@gmail.com>
2024-11-12 19:12:51 +0100

    remove comments

--- src/newproducts/old.product.ts
+++ src/newproducts/old.product.ts
@@ -8,7 +8,6 @@ export class OldProduct {
 
   desc: string;
   longDesc: string;
-  // warehouse stock has to be separate too
   counter: number | null;
 
   constructor(
@@ -23,47 +22,35 @@ export class OldProduct {
     this.counter = counter;
   }
 
-  // Probably, this method will be moved to warehouse stock class
   decrementCounter(): void {
     if (this.price.isNonZero()) {
-      // counter should be validated in separate class
-      // taking item from warehouse
       if (this.counter === null) {
         throw new NotAcceptableException('null counter');
       }
       this.counter = this.counter - 1;
-      // throw the error when warehouse has negative amount of items
       if (this.counter < 0) {
         throw new NotAcceptableException('Negative counter');
       }
     }
   }
 
-  // Probably, this method will be moved to warehouse stock class
   incrementCounter(): void {
     if (this.price.isNonZero()) {
-      // when empty stock then throw an error
       if (this.counter === null) {
         throw new NotAcceptableException('null counter');
       }
-      // add item to stock
       if (this.counter + 1 < 0) {
         throw new NotAcceptableException('Negative counter');
       }
-      // and item to warehouse stock
       this.counter = this.counter + 1;
     }
   }
 
-  // Probably, this method will be moved to money class
   changePriceTo(newPrice: Price): void {
-    // if warehouse stock doesn't have any items throw the error
     if (this.counter === null) {
       throw new NotAcceptableException('null counter');
     }
-    // when warehouse has some items
     if (this.counter > 0) {
-      // price should be validated in separate class
       if (newPrice === null) {
         throw new NotAcceptableException('new price null');
       }
@@ -71,36 +58,28 @@ export class OldProduct {
     }
   }
 
-  // i think it should be Description class
   replaceCharFromDesc(charToReplace: string, replaceWith: string): void {
-    // if statement is repeating
     if (
       this.longDesc === null ||
       this.longDesc.length === 0 ||
       this.desc === null ||
       this.desc.length === 0
     ) {
-      // when description or long description is not provided
       throw new NotAcceptableException('null or empty desc');
     }
-    // replace text
     this.longDesc = this.longDesc.replace(charToReplace, replaceWith);
     this.desc = this.desc.replace(charToReplace, replaceWith);
   }
 
-  // i think it should be Description class
   formatDesc(): string {
-    // if statement is repeating
     if (
       this.longDesc == null ||
       this.longDesc.length === 0 ||
       this.desc == null ||
       this.desc.length === 0
     ) {
-      // when description and long description are empty return nothing
       return '';
     }
-    // return formatted description
     return this.desc + ' *** ' + this.longDesc;
   }
 
